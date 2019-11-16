436333d5f672f1379f1a3a7cebb09a2f508ae2ba
MYcanada <57339131+MYcanada@users.noreply.github.com>
2019-11-16 01:51:05 -0500

    Add files via upload

--- chapter3Exercise4.java
+++ chapter3Exercise4.java
@@ -10,21 +10,25 @@ public class chapter3Exercise4{
     public static void main(String[] args) {
         //pick a random number
         Random random = new Random();
-        int UserGuess;
+        int guess, difference;
         int number = random.nextInt(100) + 1;
         
         System.out.println("I'm thinking of a number between 1 and 100");
         System.out.println("(including both). Can you guess what it is?");
         
+        System.out.println("Type a number: ");
         Scanner in = new Scanner(System.in);
-        int guess = in.nextInt();
+        guess = in.nextInt();
         System.out.println("Your guess is " + guess);
         
         System.out.println("The number I was thinking of is " + number);
         
-        int difference = guess - number;
-        System.out.println("You were off by: " + difference);
-        
+        if (guess != number) {
+            difference = guess - number;
+            System.out.println("You were off by: " + difference);
+        } else if (guess == number) {
+            System.out.println("Your guess was correct.");
+        }
             
     } 
 
